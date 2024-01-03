8117599feebcf73126823bed182d1cfa51df4a7c
Golhun <geraldduah1@gmail.com>
2024-01-03 10:59:30 +0000

    Update 06 - islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,11 +4,8 @@
 def islower(c):
     if not c:
         return False
-    
-    
     ascii_value = ord(c)
     if ascii_value >= ord("a") and ascii_value <= ord("z"):
         return True
     else:
         return False
-    
