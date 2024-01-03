116593f070806f0d6e676e5e8adbbe32f1638129
Golhun <geraldduah1@gmail.com>
2024-01-03 09:54:00 +0000

    Update - Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 
 
 for tens in range(0, 10):
-    for ones in range(tens + 1, 9):
+    for ones in range(tens + 1, 10):
         print("{:d}{:d}".format(tens, ones),
               end=", " if tens < 9 or ones < 8 else "\n")
