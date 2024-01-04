46d3a82b142021dfabf82581d8afa35cd665bd34
Golhun <geraldduah1@gmail.com>
2024-01-04 02:05:07 +0000

    the popular FizzBuzz program

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    for number in range(0, 100 + 1):
+        if number % 5 and number % 3 == 0:
+            print("FizzBuZZ", end=", ")
+        elif number % 3 == 0:
+            print("Fizz", end=", ")
+        elif number % 5 == 0:
+            print("Buzz", end=", ")
+        else:
+            print(number, end=", ")
