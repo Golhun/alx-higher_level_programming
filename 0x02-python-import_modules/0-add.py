c753ce3fe588b2ed1c81d8279b29642fc90d4ab4
Golhun <geraldduah1@gmail.com>
2024-01-04 22:26:42 +0000

    a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    def add(a, b):
+        return a + b
+
+    a = 1
+    b = 2
+
+    result = add(a, b)
+
+    print("{} + {} = {}".format(a, b, result))
