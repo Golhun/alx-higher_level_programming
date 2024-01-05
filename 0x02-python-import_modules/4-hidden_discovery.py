2556a7714cff14bc54e6cc66a68bc42081740132
Golhun <geraldduah1@gmail.com>
2024-01-05 18:16:29 +0000

    made corrections to hidden discovery file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,9 @@
 import hidden_4
 
 if __name__ == "__main__":
-    module_names = [name for name in dir(hidden_4) if not name.startswith("__")]
-    
+    module_names = [
+        name for name in dir(hidden_4) if not name.startswith("__")
+        ]
+
     for name in sorted(module_names):
         print(name)
