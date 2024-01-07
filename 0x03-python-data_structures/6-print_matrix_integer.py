5b01ab34780676cd170bc36fb92552e5662280ca
Golhun <geraldduah1@gmail.com>
2024-01-07 17:53:53 +0000

    Update 00 -  a function that prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,9 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    for item in matrix: # get the sublists
-        for i,value in enumerate(item): # for every sublist we treat it as a row or an individual list with th enumerate() function
-            print("{:d}".format(value), end="") # then we print the values in that list
-            if i < (len(item) - 1): # we check if the current element is the last element 
-                print(" ", end="") # if it isn't we print a space and keep the next element on the same line with end=""
-        print() # we now print a new line before the main for loop runs again
+    for item in matrix:
+        for i, value in enumerate(item):
+            print("{:d}".format(value), end="")
+            if i < (len(item) - 1):
+                print(" ", end="")
+        print()
