f648700303e95fd0f8a2d2efe7c82262225f99cb
Golhun <geraldduah1@gmail.com>
2024-01-07 16:49:28 +0000

    Looked at python list methods

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    matrix_items = matrix[0]
+    for item in matrix_items:
+        pass
---
+++ 0x03-python-data_structures/python_nested_list.py
@@ -0,0 +1,30 @@
+my_list = [3.5, 10, "code", [1, 2, 3], 8]
+
+# add element to the end of the list
+# my_list.append(12)
+# print('{}'.format(my_list))
+
+# add element at a specific position in a list
+# my_list.insert(0, "Hello")
+# print("{}".format(my_list))
+
+# remove the first occurrence of a specified element from a list
+# my_list.remove("code")
+# print("{}".format(my_list))
+
+# remove element at a specified position in a list and return it
+# element = my_list.pop(2)
+# print("{}".format(element))
+# print("{}".format(my_list))
+
+# sort elements in a list in ascending order and returns it. (form highest to lowest)
+# my_list = [0, 3, 5, 7, 0, 3, 5, 5]
+# ascending_order = sorted(my_list) # this line sorts the list without modifying the original list
+# print("{}".format(ascending_order))
+# my_list.sort() # this line sorts the list by modifying the original list
+# print("{}".format(my_list))
+
+# reverse the order of a list
+print("{}: {}\n".format("Original list", my_list))
+my_list.reverse()
+print("{}: {}".format("Reversed list", my_list))
