3974803131635835e774bfc8dc984794ee3da243
Golhun <geraldduah1@gmail.com>
2024-01-24 03:23:32 +0000

    pycodestyle correction 00 - a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,6 @@
 class Square():
     """A square class that defines a square"""
 
-
     def __init__(self, size):
-        """The init method that initializes our size attribute as a private attribute"""
+        """initializing our size attribute as a private attribute"""
         self.__size = size
