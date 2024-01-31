2cedf1baafb65483d6cd0b6ed09ac9fa980ee660
Golhun <geraldduah1@gmail.com>
2024-01-31 03:26:18 +0000

    a function that prints a text with 2 new lines after each of these characters: ., ? and :
    Function is tested using doctest module

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module that prints a text with 2 new lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    chars = (".", "?", ":")
+    final_text = ""
+    
+    for char in text:
+        if char in chars:
+            final_text += f"{char}$\n$\n"
+        else:
+            final_text += char
+    
+    print(final_text)
