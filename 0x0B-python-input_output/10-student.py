444cd8a60399427715abaf5e815ae91ed4e08199
Golhun <geraldduah1@gmail.com>
2024-02-06 13:56:54 +0000

    pycodestyle correction  for 10-student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -27,13 +27,17 @@ class Student:
         Retrieves a dictionary representation of a Student instance.
 
         Args:
-            attrs (list of str, optional): A list of attribute names to retrieve. 
-                If None, retrieve all attributes. Default is None.
+            attrs (list of str, optional):
+            A list of attribute names to retrieve.
+            If None, retrieve all attributes.
+            Default is None.
 
         Returns:
-            dict: A dictionary containing the specified attributes of the Student instance.
+            dict:
+            A dict containing specified attributes of Student instance.
         """
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
