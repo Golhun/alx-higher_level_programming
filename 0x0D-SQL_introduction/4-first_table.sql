5d719ff4e7f4e0652c6b7d452e5e7aa029db4029
gerry_bron <geraldduah1@gmail.com>
2024-04-15 23:43:51 +0000

    add excercise 4. First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- This script creates the table first_table if it doesn't already exist
+
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
