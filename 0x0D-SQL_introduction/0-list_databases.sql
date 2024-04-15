49ee76064e412f768118baa325f7da7413ab76c0
gerry_bron <geraldduah1@gmail.com>
2024-04-15 23:14:44 +0000

    update readme and 0. List databases

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,4 @@
+-- This script lists all the databases on the sql server
+
+-- Listing all the databases on the server
+SHOW DATABASES
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,2 @@
+# Introduction to Structured Query Language (SQL)
+Welcome to our first steps in learning sql and Mysql. Come along.
