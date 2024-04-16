b2d6a1d3f0c31641e6971fd9be8079ec62c50778
gerry_bron <geraldduah1@gmail.com>
2024-04-16 00:20:07 +0000

    add excercise 10. List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- This script lists all records of the second_table, ordered by score (top first)
+
+-- Select all records from the second_table, ordering by score (top first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
