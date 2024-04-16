54efcf0f83f56607809ae30fa66f5a275b0b7069
gerry_bron <geraldduah1@gmail.com>
2024-04-16 00:02:07 +0000

    add excercise 8. Count 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- This script counts the number of records with id = 89 in the first_table
+
+-- Count the number of records with id = 89
+SELECT COUNT(*) AS count_89
+FROM first_table
+WHERE id = 89;
