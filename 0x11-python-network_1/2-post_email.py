c580ec31be1746ba0ca7d37e597a82842170212b
Gerald Adu-Broni <geraldduah1@gmail.com>
2024-05-11 19:33:49 +0000

    added new excercise

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""Sends a POST request with a variable 'email' to a specified URL."""
+"""Sends a POST request with an 'email' parameter to a specified URL."""
 
-import urllib.request
-import urllib.parse
 import sys
+import urllib.parse
+import urllib.request
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Get the URL and email from command-line arguments
     url = sys.argv[1]
     email = sys.argv[2]
 
     # Prepare the data to be sent in the POST request
     values = {'email': email}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
+    data = urllib.parse.urlencode(values).encode("ascii")
 
     # Create and send the POST request
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        # Read and print the response
-        res = response.read()
-        print(res.decode('utf-8'))
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        # Display the body of the response
+        print(response.read().decode("utf-8"))
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Sends a request to a URL and displays the body of the response (decoded in utf-8).
+"""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        # Send a request to the specified URL
+        with request.urlopen(sys.argv[1]) as res:
+            # Display the body of the response
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        # Handle HTTP errors if they occur
+        print('Error code:', er.code)
