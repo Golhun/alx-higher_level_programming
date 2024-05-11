a79755b4c9f781394a690ecd2c7eb4d0fca50b3d
Gerald Adu-Broni <geraldduah1@gmail.com>
2024-05-11 14:02:12 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,8 +1,21 @@
 #!/usr/bin/node
-const Rectangle = require('./3-rectangle');
+class Rectangle {
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
 
-const r1 = new Rectangle(2, 3);
-r1.print();
+	print() {
+		for (let i = 0; i < this.height; i++) {
+			let s = "";
+			for (let j = 0; j < this.width; j++) {
+				s += "X";
+			}
+			console.log(s);
+		}
+	}
+}
 
-const r2 = new Rectangle(10, 5);
-r2.print();
+module.exports = Rectangle;
